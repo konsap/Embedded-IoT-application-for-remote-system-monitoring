287bd1ed2ccca03f4a9a946495c64994b0db93a1
konsap <138793681+konsap@users.noreply.github.com>
2023-07-31 17:06:04 +0300

    Delete microcontroller application/synergy_cfg/synergy_cfg/ssp_cfg directory

--- microcontroller application/synergy_cfg/synergy_cfg/ssp_cfg/driver/r_elc_cfg.h
+++
@@ -1,5 +0,0 @@
-/* generated configuration header file - do not edit */
-#ifndef R_ELC_CFG_H_
-#define R_ELC_CFG_H_
-#define ELC_CFG_PARAM_CHECKING_ENABLE (1)
-#endif /* R_ELC_CFG_H_ */
--- microcontroller application/synergy_cfg/synergy_cfg/ssp_cfg/framework/el/fx_user.h
+++
@@ -1,7 +0,0 @@
-/* generated configuration header file - do not edit */
-#ifndef FX_USER_H_
-#define FX_USER_H_
-#if (SYNERGY_NOT_DEFINED)
-#include "fx_src_user.h"
-#endif
-#endif /* FX_USER_H_ */
